b007505d291e85f8a2f61517acf258792c5c7a91
Venkatesan C <108179969+imvenkatesanc@users.noreply.github.com>
2023-08-24 19:47:11 +0530

    Add files via upload

---
+++ src/main/java/com/mrc/internapp/InternappApplication.java
@@ -0,0 +1,13 @@
+package com.mrc.internapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternappApplication.class, args);
+	}
+
+}
